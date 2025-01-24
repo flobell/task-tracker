267a7666753ce45bd3c8ba64fec59a3511f8cc94
Pedro Flores <manuelflores1795@gmail.com>
2025-01-24 08:49:36 -0600

    Fix to easy installation

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    version='1.0.0',
+    name="task-tracker",
+    version="1.0.0",
+    description="A command-line tool for managing your tasks efficiently",
     packages=find_packages(),
     install_requires=None,
+    author="Pedro",
+    author_email="manuelflores1795@gmail.com",
+    url="https://github.com/flobell/task-tracker.git",
+    py_modules=["src"],
     entry_points={
         'console_scripts': [
-            'task-cli = src.cli.commands:commands',
+            'task-cli=src.cli.commands:commands',
         ],
     },
+    tests_require=[
+        "unittest",
+    ],
+    python_requires=">=3.9",
 )
